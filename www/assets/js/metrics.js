e524fb306e786dc85ffd22f8a50911980fb3f854
Greg Ledbetter <gsledbetter@gmail.com>
2013-05-01 20:43:37 -0400

    Common storage for version number, build, date and STD data date.

--- www/assets/js/metrics.js
+++ www/assets/js/metrics.js
@@ -1,9 +1,11 @@
+var appData = { "version":"1.0.4", "build":"3", "buildDate":"5/1/2013 ", "stdDataDate":"8/9/2012"};
+
 function trackPageView (section, page)
 {
         // console.log("In trackPageView");
 
 		// these first vars change most often depending on version and if debug is true
-		var appVersion = "1.0.3.3";
+		var appVersion = appData.version + "." + appData.build;
 		var debug = true;
 		var debugLocal = true;
 		var cdcServer = "http://tools.cdc.gov/metrics.aspx?";
